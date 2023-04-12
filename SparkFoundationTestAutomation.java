84d74ecd9b23993458a302bfe5d08c7213e020f7
Aditya Tiwary <83766476+Aditya-Tiwary@users.noreply.github.com>
2023-04-12 15:59:04 +0530

    Update SparkFoundationTestAutomation.java

--- SparkFoundationTestAutomation.java
+++ SparkFoundationTestAutomation.java
@@ -14,6 +14,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class SparkFoundationTestAutomation {
     
@@ -26,36 +29,65 @@ public class SparkFoundationTestAutomation {
         WebDriver driver = new ChromeDriver();
 
         // Navigate to the website
-        driver.get("https://thesparksfoundation.sg");
+        driver.get("https://www.thesparksfoundationsingapore.org/");
 
         // Wait for the page to load
-      Duration timeout = Duration.ofSeconds(10);
-WebDriverWait wait = new WebDriverWait(driver, timeout);
+       Duration timeout = Duration.ofSeconds(10);
+       WebDriverWait wait = new WebDriverWait(driver, timeout);
 
         wait.until(ExpectedConditions.titleContains("The Sparks Foundation"));
-
-        // Check the logo exists
+       System.out.println("Page 1: Home Page");
+        //Test 1 Check the logo exists
+        System.out.println("Test Case 1:");
         WebElement logo = driver.findElement(By.className("navbar-brand"));
         if (logo.isDisplayed()) {
             System.out.println("Logo exists");
         }
-
-        // Check the navigation bar appears
-        WebElement navBar = driver.findElement(By.id("navbar"));
-        if (navBar.isDisplayed()) {
-            System.out.println("Navigation bar appears");
+        else {
+        System.out.println("Logo does not exist!\n");
         }
-
-        // Check the name on about us page is correct
-        driver.findElement(By.linkText("About Us")).click();
-        WebElement aboutUs = driver.findElement(By.tagName("h4"));
-        if (aboutUs.getText().equals("About Us")) {
-            System.out.println("Name on About Us page is correct");
+        
+        //Test 2: Title Verification
+        System.out.println("Test Case 2:");
+        if (driver.getTitle() != null) {
+        System.out.println("Title Verification Successful: " + driver.getTitle());
+        } else {
+        System.out.println("Title Verification Failed!\n");
         }
-
-        // Check other elements on different pages
-        // ...
-
+        
+        // Test Case 3: Verify Navigation Menu Items
+        System.out.println("Test Case 3:");
+       List<WebElement> navigationMenuItems = driver.findElements(By.cssSelector(".navbar-nav > li"));
+       if (navigationMenuItems.size() == 6) {
+       System.out.println("Navigation menu items count is correct");
+       List<String> expectedMenuItems = Arrays.asList("About Us", "Policies and Code", "Programs", "LINKS", "Join Us", "Contact Us");
+       List<String> actualMenuItems = new ArrayList<>();
+       for (WebElement menuItem : navigationMenuItems) {
+        actualMenuItems.add(menuItem.getText().trim());
+       }
+       if (actualMenuItems.equals(expectedMenuItems)) {
+        System.out.println("Navigation menu items are correct");
+        } else {
+        System.out.println("Navigation menu items are incorrect");
+        }
+      } else {
+      System.out.println("Navigation menu items count is incorrect");
+      }
+      // Test Case 4: Verify Images on Home Page
+      List<WebElement> images = driver.findElements(By.tagName("img"));
+      
+      if (images.size() == 1) {
+      System.out.println("Image count is correct");
+      for (WebElement image : images) {
+        if (image.getAttribute("src").isEmpty()) {
+            System.out.println("Image source is empty");
+        } else {
+            System.out.println("Image source is not empty");
+        }
+        }
+       } else {
+       System.out.println("Image count is incorrect");
+       }
         // Close the browser
         driver.quit();
     }
